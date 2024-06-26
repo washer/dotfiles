1d6b2bd9f63eca3867b4a50a08de78557aad8240
Mads Steensig Pelt <msp@connectedcars.dk>
2024-06-26 15:37:38 +0200

    add time to nowplaying output

--- nvim/lua/tools/nowplaying.lua
+++ nvim/lua/tools/nowplaying.lua
@@ -1,11 +1,19 @@
 local Job = require("plenary.job")
+local math = require("math")
+local string = require("string")
 
-local current_song = ""
+local binary_installed = false
+local current_title = ""
 local current_artist = ""
+local current_time = 0
 
 local M = {}
 
--- TODO: Add a check for whether the nowplaying-cli binary is installed and fail gracefully and vim.notify if not
+local function disp_time(time)
+	local minutes = math.floor((time % 3600) / 60)
+	local seconds = math.floor(time % 60)
+	return string.format("%02d:%02d", minutes, seconds)
+end
 
 local function start_artist_job()
 	Job:new({
@@ -37,8 +45,48 @@ local function start_title_job()
 	}):start()
 end
 
+local function start_time_job()
+	Job:new({
+		command = "nowplaying-cli",
+		args = { "get", "elapsedTime" },
+		on_stdout = function(j, data)
+			if data then
+				current_time = data
+			end
+		end,
+		on_exit = function()
+			start_time_job()
+		end,
+	}):start()
+end
+
+local function init_nowplaying()
+	if binary_installed == false then
+		return
+	end
+	start_artist_job()
+	start_title_job()
+	start_time_job()
+end
+
+local function verify_binary()
+	Job:new({
+		command = "which",
+		args = { "nowplaying-cli" },
+		on_exit = function(j, return_val)
+			if return_val ~= 0 then
+				vim.notify("nowplaying-cli is not installed", vim.log.levels.ERROR)
+			else
+				binary_installed = true
+				init_nowplaying()
+			end
+		end,
+	}):start()
+end
+
 function M.getCurrentlyPlaying()
-	return current_artist .. " - " .. current_title
+	-- TODO: add check for what parts we have and make it nice if we only have title
+	return current_artist .. " - " .. current_title .. " | " .. disp_time(current_time)
 end
 
 function M.nextSong()
@@ -55,6 +103,5 @@ function M.previousSong()
 	}):start()
 end
 
-start_artist_job()
-start_title_job()
+verify_binary()
 return M
