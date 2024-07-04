e87c20cf42b487889eaf48b24d8de18ad2ba8e5e
Mads Steensig Pelt <msp@connectedcars.dk>
2024-07-04 10:36:11 +0200

    some improvements to nowplaying stuff

--- nvim/lua/tools/nowplaying.lua
+++ nvim/lua/tools/nowplaying.lua
@@ -3,6 +3,7 @@ local math = require("math")
 local string = require("string")
 
 local binary_installed = false
+local currently_playing = false
 local current_title = ""
 local current_artist = ""
 local current_time = 0
@@ -13,6 +14,33 @@ local function disp_time(time)
 	local minutes = math.floor((time % 3600) / 60)
 	local seconds = math.floor(time % 60)
 	return string.format("%02d:%02d", minutes, seconds)
+end
+
+local function get_playing_state_symbol()
+	if currently_playing == true then
+		return "▶"
+	elseif currently_playing == false then
+		return "⏸"
+	else
+		return ""
+	end
+end
+
+local function start_state_job()
+	Job:new({
+		command = "nowplaying-cli",
+		args = { "get", "playbackRate" },
+		on_stdout = function(j, data)
+			if data == "1" then
+				currently_playing = true
+			else
+				currently_playing = false
+			end
+		end,
+		on_exit = function()
+			start_state_job()
+		end,
+	}):start()
 end
 
 local function start_artist_job()
@@ -45,11 +73,16 @@ local function start_title_job()
 	}):start()
 end
 
+-- TODO: It would be nicer if this could just run async once a second
 local function start_time_job()
 	Job:new({
 		command = "nowplaying-cli",
 		args = { "get", "elapsedTime" },
 		on_stdout = function(j, data)
+			if currently_playing == false then
+				return
+			end
+
 			if data then
 				current_time = data
 			end
@@ -64,6 +97,7 @@ local function init_nowplaying()
 	if binary_installed == false then
 		return
 	end
+	start_state_job()
 	start_artist_job()
 	start_title_job()
 	start_time_job()
@@ -86,7 +120,13 @@ end
 
 function M.getCurrentlyPlaying()
 	-- TODO: add check for what parts we have and make it nice if we only have title
-	return current_artist .. " - " .. current_title .. " | " .. disp_time(current_time)
+	return get_playing_state_symbol()
+		.. " "
+		.. current_artist
+		.. " - "
+		.. current_title
+		.. " | "
+		.. disp_time(current_time)
 end
 
 function M.nextSong()
